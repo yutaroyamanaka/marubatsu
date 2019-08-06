3757c8755276e38b61e377ab600d4a1bff8c3b19
yutaroyamanaka <falcon4842@gmail.com>
2019-08-06 23:55:54 +0900

    🚀 Stateless

--- backend/api/api_test.py
+++ backend/api/api_test.py
@@ -3,7 +3,6 @@ import api
 import sys
 import json
 import random
-sys.path.append("/marubatsu/backend/")
 
 
 class TestGetAPIClient(unittest.TestCase):
@@ -11,44 +10,27 @@ class TestGetAPIClient(unittest.TestCase):
     def setUp(self):
         self.app = api.app.test_client()
 
-    def test_start(self):
-        res = self.app.get("/api/start")
-        data = json.loads(res.data)
-        assert res.status_code == 200
-        assert data["finish"] is False
-        assert data["player1-win"] is False
-        assert data["player2-win"] is False
-
-    def test_reset(self):
-        res = self.app.get("/api/reset")
-        data = json.loads(res.data)
-        assert res.status_code == 200
-        self.assertNotIn('X', data["board"])
-        self.assertNotIn('O', data["board"])
-
-
-class TestPlayAPI(unittest.TestCase):
-
-    def setUp(self):
-        self.app = api.app.test_client()
-        self.app.get("/api/reset")
-        self.app.get("/api/start")
-
-    def test_play_same_moves(self):
-        board = json.loads((self.app.get("/api/board/info")).data)["board"]
-        choice = random.choice([i+1 for i in range(len(board)) if board[i] == ' '])
+    def test_best_action(self):
         res = self.app.post("/api/play", data=json.dumps(dict(
-            idx=str(choice)
-        )), content_type='application/json')
+            board=["X", "X", "O", " ", "O", " ", " ", " ", " "],
+            char = "X"
+        )), content_type="application/json")
+
         data = json.loads(res.data)
         assert res.status_code == 200
-        assert data["board"][choice-1] == 'X'
+        assert data["end"] is True
+        assert data["win"] is True
 
+    def test_game_over(self):
         res = self.app.post("/api/play", data=json.dumps(dict(
-            idx=str(choice)
+            board=["X", "O", "O", "X", "O", " ", "X", " ", " "],
+            char="O"
         )), content_type='application/json')
-        assert res.status_code == 500
 
+        data = json.loads(res.data)
+        assert res.status_code == 200
+        assert data["end"] is True
+        assert data["win"] is False
 
 if __name__ == '__main__':
     unittest.main()
